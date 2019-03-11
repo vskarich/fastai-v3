48b973470e533894a88fedb5eafd498ee38c64d8
Veljko Skarich <veljko@swarm.enterprises>
2019-03-11 16:04:24 -0700

    Change static path for prod.

--- app/server.py
+++ app/server.py
@@ -17,7 +17,7 @@ path = pathlib.Path(__file__).parent
 
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
-app.mount('/static', StaticFiles(directory='static'))
+app.mount('app/static', StaticFiles(directory='static'))
 
 async def download_file(url, dest):
     if dest.exists(): return
